270997c801f22ff3ce1f2baf4846c7ff219b3aee
Renato S Moreno <renatomorenocz@gmail.com>
2018-04-14 09:06:58 -0300

    Init

---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import logo from '../logo.svg'
+
+export default () => (
+  <header className='green tc pv4 pv5-ns'>
+    <img src={logo} className='br-100 pa1 ba b--black-10 h3 w3' alt='avatar' />    
+  </header>
+)
