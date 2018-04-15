25d23ae0077f7e83d2a9b70ac3e7efc6aeda2362
Renato S Moreno <renatomorenocz@gmail.com>
2018-04-15 11:27:29 -0300

    Remove the Header component

--- src/components/header.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import logo from '../images/logo.svg'
-
-export default () => (
-  <header className='green tc pv4 pv5-ns'>
-    <img src={logo} className='br-100 pa1 ba b--black-10 h3 w3' alt='avatar' />    
-  </header>
-)
