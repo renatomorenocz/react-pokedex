63b4f56252f9f0d4e74426077471fc156eb83b2f
Renato S Moreno <renatomorenocz@gmail.com>
2018-04-16 07:45:49 -0300

    Fix images resources fallback

--- src/components/PokemonImage.js
+++ src/components/PokemonImage.js
@@ -4,24 +4,27 @@ import { getIdPokemonByUrl } from '../utils/helpers';
 import LazyLoad from 'react-image-lazy-load';
 import imagePlaceholder from '../images/placeholder.gif';
 
-const PokemonImage = ({ pokemon, width, height }) => (
-  <div>
-    <LazyLoad
-      height={height}
-      loaderImage={true}
-      originalSrc={
-        'https://assets.pokemon.com/assets/cms2/img/pokedex/full/' +
-        getIdPokemonByUrl(pokemon.url) +
-        '.png'
-      }
-      imageProps={{
-        src: imagePlaceholder,
-        alt: pokemon.name,
-        width: width
-      }}
-    />
-  </div>
-);
+const PokemonImage = ({ pokemon, width, height }) => {
+  let urlImage =
+    getIdPokemonByUrl(pokemon.url) < 1001
+      ? 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/'
+      : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+  return (
+    <div>
+      <LazyLoad
+        height={height}
+        loaderImage={true}
+        originalSrc={urlImage + getIdPokemonByUrl(pokemon.url) + '.png'}
+        imageProps={{
+          src: imagePlaceholder,
+          alt: pokemon.name,
+          width: width
+        }}
+      />
+    </div>
+  );
+};
 
 PokemonImage.propTypes = {
   pokemon: PropTypes.object.isRequired,
